94f15e1570e394f8f0531336aca1f2dc98b2ad27
meschya <meschyaaa@gmail.com>
2022-02-12 18:18:58 +0300

    Initial Commit

---
+++ Currency Converter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Currency Converter
+//
+//  Created by Egor Mesheryakov on 12.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
